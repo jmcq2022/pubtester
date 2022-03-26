32b8974d48772e205dfbc4205e279fcf7b41056d
james <jamesmcquade039@gmail.com>
2022-03-26 00:13:34 +0000

    counts top 100 instances of words

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pdfminer
 ##remember to install pdfminer.
+import collections
 
 from io import StringIO
 from pdfminer.converter import TextConverter
@@ -18,8 +19,36 @@ with open('02488082.pdf', 'rb') as in_file:
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     for page in PDFPage.create_pages(doc):
       interpreter.process_page(page)
-print(output_string.getvalue())
+#print(output_string.getvalue())
+string = output_string.getvalue()
 
+
+#figuring out how to count each word
+arr = [ x.strip() for x in string.strip().split(' ') ]
+
+#print (arr)
+#print (arr.count('the'))
+x = len(arr)
+#print(x)
+i=0
+n = len(arr)
+# function to count
+
+print(collections.Counter(arr).most_common(100))
+
+
+
+
+#while i < len(arr):
 
 ##formula is below,
 #pt = companies trustworthyness . persons trustworthniess . countries trusthworthiness(lenght of report/average report length for similar firms . (buzzwords/paragraph + amount of esg information / total length of report + pictures/total words))
+
+#things to need to be done
+#Length of report
+#Buzzword counter
+#total paragraphs
+#total amount of ESG buzzwords
+#Total amount of words
+#total number of pics
+#total number of words
