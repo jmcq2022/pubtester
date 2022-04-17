9810fdab281431a3898964646441eec8d2964ecd
james <jamesmcquade039@gmail.com>
2022-04-17 11:32:49 +0100

    fixed main.py,

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
-import matplotlib
+import matplotlib.pyplot as plt
 from os import path
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
@@ -22,7 +22,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 def arraycreator():
     output_string = StringIO()
-    with open('02488082.pdf', 'rb') as in_file:
+    with open('warhammer.pdf', 'rb') as in_file:
         parser = PDFParser(in_file)
         doc = PDFDocument(parser)
         rsrcmgr = PDFResourceManager()
@@ -65,14 +65,8 @@ def entirefilter(wordarray):
     print(collections.Counter(wordarray))
     return()
 
-#arraycreator()
-#wordarray=wordstripper(arraycreator())
 
-
-#mostcommon100(wordstripper(arraycreator()))
-#mostcommon100(arraycreator())
-#entirefilter(arraycreator())
-
+#this is the smartness that converts the PDF information into it. Needs to be passed a BSID
 def wordwriter(bsid, wordarray):
     df = p.read_csv('wordcount.csv')
     #print(df)
@@ -91,8 +85,29 @@ def wordwriter(bsid, wordarray):
         df2.to_csv('wordcount.csv')
         print('Added length  to dataframe')
 
-wordwriter(103,arraycreator())
+#wordwriter(103,arraycreator())
 #index,word,count,bsid
 
 #print(collections.Counter(arraycreator()))
 
+wordwriter(350,arraycreator())
+
+
+#needs the bullshitID to work. Filters the wordcount.csv based on it.
+def wordcloud(bsid):
+    #grab word+frequency
+    wordcountdf = p.read_csv('wordcount.csv',index_col=[0])
+    wordcountdffiltered = wordcountdf[wordcountdf['bsid']==bsid]
+    wordcountdffiltered = wordcountdffiltered[["word","count"]]
+    print(wordcountdffiltered)
+    text = " ".join(review for review in wordcountdffiltered.word.astype(str))
+    wordcloud = WordCloud(width=1280, height=700).generate_from_text(text)
+    plt.axis("off")
+    plt.figure(figsize=(40, 20))
+    plt.tight_layout(pad=0)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.show()
+
+
+
+wordcloud(350)
